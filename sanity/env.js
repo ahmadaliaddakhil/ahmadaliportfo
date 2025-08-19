40976e0b08432047946a26938ad26ce28add9ba4
ahmadaliaddakhil <ahmadaliaddakhil@gmail.com>
2025-08-20 05:04:04 +0700

    add sertif

--- sanity/env.js
+++ sanity/env.js
@@ -1,4 +1,4 @@
-export const apiVersion = "2024-02-07"; // Pastikan versi API sesuai
-export const dataset = "production"; // Ganti dengan nama dataset Anda
-export const projectId = "py8yus6z"; // Ganti dengan project ID Anda
-export const useCdn = false; // Biasanya false untuk data selalu terbaru
+export const apiVersion = '2024-02-07'
+export const dataset = 'production'
+export const projectId = 'py8yus6z'
+export const useCdn = false
--- src/app/commons/constants/CertificateLists/index.js
+++ src/app/commons/constants/CertificateLists/index.js
@@ -8,6 +8,40 @@ export const CERTIFICATELISTS = [
     diberikan: "Nov 11, 2023",
     berlaku: "death",
     by: "kemendikbud",
-    slug: "belajar-melakukan-penelitian-pemula",
+    slug: "opsi-puspresnas",
+  },
+  {
+    title:
+      "Asean Innovative Scienve Environmental And Entrepreuneur Fair (AISEEF)",
+    image:
+      "https://res.cloudinary.com/dpkmhts3q/image/upload/v1755640761/sertif1_1_zk7gbj.jpg",
+    description:
+      "research competition for all students in international level, and I researched in the field of Internet Of Things (IoT).",
+    diberikan: "Feb 5, 2024",
+    berlaku: "death",
+    by: "IYSA-UNDIP",
+    slug: "AISEEF-IYSA-UNDIP",
+  },
+  {
+    title: "Indonesia International Applied Science Project Olympiad (I2ASPO)",
+    image:
+      "https://res.cloudinary.com/dpkmhts3q/image/upload/v1755640771/sertif2_1_zhyb47.jpg",
+    description:
+      "research competition for all students in international level, and I researched in the field of Internet Of Things (IoT).",
+    diberikan: "Dec 21, 2024",
+    berlaku: "death",
+    by: "IYSA-ITS",
+    slug: "AISEEF-IYSA-ITS",
+  },
+  {
+    title: "Lomba Kompetensi Siswa (LKS)",
+    image:
+      "https://res.cloudinary.com/dpkmhts3q/image/upload/v1755640795/sertif3_2_vqodml.jpg",
+    description:
+      "skill competition for all students in regency level, and I competed in the field of Artificial Intelligence. (AI)",
+    diberikan: "Feb 25, 2025",
+    berlaku: "death",
+    by: "IYSA-ITS",
+    slug: "Puspresnas",
   },
 ];
